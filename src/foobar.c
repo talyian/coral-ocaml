2d6a056f4c0329105904867bc2b98535360f0c8e
Jimmy <talyian@gmail.com>
2020-12-02 00:58:19 -0500

    .

--- src/foobar.c
+++ src/foobar.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <stdarg.h>
+#include <string.h>
 #define PCRE2_CODE_UNIT_WIDTH 0
 #include <pcre2.h>
 
@@ -14,4 +16,50 @@ int32_t i32_l(int64_t v) {
 
 int32_t i32_s(char * v) {
   return atol(v);
+}
+
+char * chars = 0, * chars_cur = 0;
+size_t chars_len = 1024;
+void put_char(char c) {
+  if (!chars) chars = chars_cur = (char*)malloc(chars_len);
+  if (chars_cur - chars >= chars_len) {
+    chars = realloc(chars, chars_len * 2);
+    chars_cur = chars + chars_len;
+    chars_len *= 2;
+  }
+  *chars_cur++ = c;
+}
+void put_string(const char * c) {
+  if (*c) {put_char(*c++); put_string(c);}
+}
+// we use this instead of printf in a test, then we can "expect" the result
+void test_printf(const char *fmt, ...) {
+  int ret;
+  va_list ap;
+  va_start(ap, fmt);
+  for(size_t i = 0, len = strlen(fmt); i < len; i++) {
+    if(fmt[i] == '%') {
+      if (i + 1 == len) { printf("format error: '%s'", fmt); exit(1); }
+      else switch(fmt[++i]) {
+        case 's': { const char * c = va_arg(ap, const char*); put_string(c); break; }
+        case 'f': {
+          char buf[20];
+          sprintf(buf, "%g", va_arg(ap, double));
+          put_string(buf);
+          break;
+        }
+        case 'd': {
+          char buf[20];
+          sprintf(buf, "%ld", va_arg(ap, int64_t));
+          put_string(buf);
+          break;
+        }
+        default: printf("format error: '%s'", fmt); exit(1);
+        }
+    } else {
+      put_char(fmt[i]);
+    }
+  }
+  va_end(ap);
+  printf("chars: %.*s", (int)chars_len, chars);
 }
--- test_wip/main.ml
+++ test_wip/main.ml
@@ -2,20 +2,20 @@ open Coral_frontend
 open Base
 
 let check_source_compiles source =
-  match Frontend.parse_string source with
-  | Ok e ->
+  try
+    match Frontend.parse_string source with
+    | Ok e ->
       let e = Coral.Init_func.run e in
       let _imports = Coral.Import_resolution.resolve e in
       let ns = Coral.Name_resolution.resolve e in
       let _ts = Coral_types.Resolver.resolve ns e in
       let ns = Coral_types.Apply_overload_reference.fix_name_resolution ns _ts in
       let _ = Coral.Llvm_backend.print_ir ns _ts e in
-      (* Stdio.print_endline @@ Ast.show_node e; *)
       true
-  | Error e ->
+    | Error e ->
       Stdio.eprintf "%s" (Frontend.show_parseError e);
       false
-  | exception _exn ->
+  with  _exn -> 
     Stdio.print_endline @@ Caml.Printexc.get_backtrace ();
     false
 
